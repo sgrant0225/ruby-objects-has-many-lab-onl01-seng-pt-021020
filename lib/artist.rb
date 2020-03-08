8d7b1519f71772bc025a28c49ecc727dd68aaccf
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-08 15:42:14 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Artist 
+ attr_accessor :name, :songs
+ 
+ 
+  def initialize(name)
+  @name = name
+  end  
+ 
+  def songs
+   Song.all.select do |song|
+     song.artist == self #self is the instance of that artist we currently working on
+   end
+  end
+   def add_song(song) #tell song it belongs to that artist
+      song.artist = self #song argument is an instance of Song
+   end 
+
+end  
+
+
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+ attr_accessor :artist, :name
+ 
+ @@all = []
+ 
+ def initialize(name)
+  @name = name
+  @@all << self 
+ end
+ 
+ def self.all #class method self is referring to the class itself 
+  @@all
+ end  
+ 
+ def artist_name
+   artist.name = artist
+ end 
+  
+end  
